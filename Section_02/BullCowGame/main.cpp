2d01adedae2167986b32db18b281f5aeabd7d0fc
Santiago Regueiro <santi@regueiro.es>
2016-01-17 00:14:48 +0100

    Lecture 14

---
+++ Section_02/BullCowGame/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	constexpr int WORD_LENGTH = 9;
+
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+
+	return 0;
+}
