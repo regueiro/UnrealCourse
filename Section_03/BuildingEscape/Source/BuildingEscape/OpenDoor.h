057c01cf9d41462d396e200d5685d0ba27aa692f
Santiago Regueiro <santi@regueiro.es>
2016-03-06 21:05:56 +0100

    Lecture 65

--- Section_03/BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ Section_03/BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -22,5 +22,10 @@ public:
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
 		
-	
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
 };
