d538bdfd06980c92a62d548b886255292f9f9de0
Santiago Regueiro <santi@regueiro.es>
2016-12-10 00:31:13 +0100

    Lecture 125

--- Section_04/BattleTank/Source/BattleTank/Tank.h
+++ Section_04/BattleTank/Source/BattleTank/Tank.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -13,6 +14,9 @@ class BATTLETANK_API ATank : public APawn
 public:
 	void AimAt(FVector HitLocation);
 
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
